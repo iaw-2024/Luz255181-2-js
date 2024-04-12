e2e4acb943494b9813ed99a260eefa597a164727
Luz255181 <mira_luz_explo@hotmail.com>
2024-04-12 14:06:18 -0300

    Terminado el ejercicio con DOM y funcionando

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+const fs = require('fs');
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '../public/express'))
+app.get("/express", (req, res) => {
+    fs.readFile('public/express/Datos.json','utf8', function(err,data){
+        if (err) {
+            console.error('Error al leer el archivo JSON:', err);
+            res.status(500).send('Error al leer el archivo JSON');
+            return;
+        }
+        const jsonData = JSON.parse(data);
+        res.render('index',{jsonData: jsonData})
+    });
+});
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static('public'))
 
